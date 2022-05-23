79e8a91d64172ee226d0e851c87080f6e95328fe
Bishoy <bishoy.samwuel@gmail.com>
2022-05-23 18:15:29 +0200

    Add referrals to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_22_123923) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_153933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_22_123923) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "referral_code"
+    t.integer "referred_by_id"
+    t.datetime "referral_completed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
