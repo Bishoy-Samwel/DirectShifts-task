2852cd1589b8b6854533824a4a92440cb3e48a79
Bishoy <bishoy.samwuel@gmail.com>
2022-05-25 14:41:06 +0200

    Update App

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -5,13 +5,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import App from './components/App';
 
-const App = () => {
-  return (
-    <div><Button variant="contained">Hello World</Button></div>
+// const App = () => {
+//   return (
+//     <div><Button variant="contained">Hello World!!</Button></div>
     
-  )
-}
+//   )
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('root'))
