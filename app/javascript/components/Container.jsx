6c7a56bcae8a04772df29de65dfb0110da71e8aa
Bishoy <bishoy.samwuel@gmail.com>
2022-05-26 17:03:34 +0200

    Skeleton For the container

---
+++ app/javascript/components/Container.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable arrow-body-style */
+/* eslint-disable import/prefer-default-export */
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { LogIn } from './Pages/LogIn';
+import { SignUp } from './Pages/SignUp';
+import { Home } from './Pages/Home';
+
+export const Container = () => {
+  const dispatch = useDispatch();
+  useEffect(() => { dispatch(loggedIn()); }, [dispatch]);
+  return (
+    <div>
+      <div id="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/logIn" element={<LogIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
