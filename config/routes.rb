fca590cb604bb5cb696a1f6a1f8fa29135d91fb9
Bishoy <bishoy.samwuel@gmail.com>
2022-05-27 21:08:08 +0200

    Testing sign in and sign up - it works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'private/test'
   get '/current_user', to: 'current_user#index'
   get '*page', to: 'components#index', constraints: -> (req) do
     !req.xhr? && req.format.html?
