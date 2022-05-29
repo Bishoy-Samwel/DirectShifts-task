aecc47721de5f16b13636b553787d0942d8e2dba
Bishoy <bishoy.samwuel@gmail.com>
2022-05-29 15:07:18 +0200

    Validation cause an error for some reason

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   before_create :set_referral_code
   after_create :complete_referral!
 
-  validates :referral_code, uniqueness: true
+  # validates :referral_code, uniqueness: true
 
   def set_referral_code
     loop do
