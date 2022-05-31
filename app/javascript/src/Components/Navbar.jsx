d7652661edf9f5f0f82213f6205e5b975ed5d38c
Bishoy <bishoy.samwuel@gmail.com>
2022-05-31 12:26:51 +0200

    Basic Navbar

---
+++ app/javascript/src/Components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import Logout from './Logout';
+
+const Navbar = ({ authChecked, loggedIn, currentUser }) => {
+  return (
+    <nav>
+      <div>
+        <div>
+          <NavLink
+            exact
+            to='/'>
+            NormalRoute
+          </NavLink>
+          <NavLink
+            exact
+            to='/protected_route'>
+            ProtectedRoute
+          </NavLink>
+          <Logout />
+        </div>
+        <div >
+          <NavLink
+            exact
+            to='/signup'>
+            Sign Up
+          </NavLink>
+          <NavLink
+            exact
+            to='/login'>
+            Log In
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
