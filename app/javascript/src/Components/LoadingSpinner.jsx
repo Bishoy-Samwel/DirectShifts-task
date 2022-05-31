cd079192f84999e815c60bf38b3fe68bf766807b
Bishoy <bishoy.samwuel@gmail.com>
2022-05-31 20:26:19 +0200

    Loading component

---
+++ app/javascript/src/Components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ImSpinner3 } from "react-icons/im";
+
+const LoadingSpinner = () => {
+  return (
+    <div>
+        <ImSpinner3 />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
