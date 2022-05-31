c5204b4719fff7bdde0dd21d3bd16385d59932b8
Bishoy <bishoy.samwuel@gmail.com>
2022-05-31 21:45:38 +0200

    Select authState, CurrentUser

--- app/javascript/src/selectors.js
+++ app/javascript/src/selectors.js
@@ -3,3 +3,8 @@ import { createSelector } from 'reselect';
 export const loggedIn = (state) => state.auth.loggedIn;
 
 export const authChecked = (state) => state.auth.authChecked;
+
+export const currentUser = (state) => state.auth.currentUser;
+
+export const authState = (state) => state.auth;
+
