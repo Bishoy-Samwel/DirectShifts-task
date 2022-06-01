7a11b9a3aecec7b220e5f13176efafae8955cc4e
Bishoy <bishoy.samwuel@gmail.com>
2022-06-01 21:19:02 +0200

    Add code , status to the response

--- app/controllers/referral_controller.rb
+++ app/controllers/referral_controller.rb
@@ -5,15 +5,16 @@ class ReferralController < ApplicationController
     referred_users_status = []
     current_user.referred_users.each do |user|
       status = user.referral_completed_at? ? "complete" : "pending"
-      referred_users_status.push({user:user, status:status})
+      referred_users_status.push({email:user.email, status:status})
     end
     return referred_users_status
   end
 
   def show
-    render json: {
+    render json: { 
+      code: 200,
       message: "Get the list of referred emails #{current_user.email}",
       referred_users_status: referred_users_status()
-    }
+    }, status: :ok
   end
 end
