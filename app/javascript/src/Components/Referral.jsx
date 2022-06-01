25dfbe39f1f0a247a67c385bda8655f833173cc8
Bishoy <bishoy.samwuel@gmail.com>
2022-06-01 14:13:15 +0200

    Generate referal link

--- app/javascript/src/Components/Referral.jsx
+++ app/javascript/src/Components/Referral.jsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { currentUser } from "../selectors";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const Referral = () => {
-    return (
-      <div >
-        Refer people you know :) 
-      </div>
-    );
+
+  const user = useSelector(currentUser)
+  const [copySuccess, setCopySuccess] = useState('');
+
+  const copyToClipBoard = async copyMe => {
+    try {
+      await navigator.clipboard.writeText(copyMe);
+      setCopySuccess('Copied!');
+    } catch (err) {
+      setCopySuccess('Failed to copy!');
+    }
+  };
+  const referral_link = `http://localhost:3000/ref:${user.referral_code}`
+  return (
+    <div >
+      Refer people you know :)
+
+      Share your personal referral link:
+      <strong id="referral_link">
+        {referral_link}
+      </strong>
+      <Button onClick={() => copyToClipBoard(referral_link)}>
+     Click here to copy
+     </Button>
+      {copySuccess}
+    </div>
+  );
 }
 
-export default Referral;
+export default Referral; 
