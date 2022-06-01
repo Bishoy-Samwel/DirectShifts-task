24d0aae394bc5fae9b613381e8f735945ac3da39
Bishoy <bishoy.samwuel@gmail.com>
2022-06-01 21:26:40 +0200

    Configure referral reducer

--- app/javascript/src/redux/configureStore.js
+++ app/javascript/src/redux/configureStore.js
@@ -4,10 +4,12 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import authReducer from './reducers/auth';
+import referralReducer from './reducers/referral';
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  referral: referralReducer,
 });
 
 export const configureStore = () => createStore(
