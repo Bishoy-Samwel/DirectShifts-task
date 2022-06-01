9a6f4983ad8cfe4b18872c60013e8631bf799405
Bishoy <bishoy.samwuel@gmail.com>
2022-06-01 11:55:36 +0200

    Add Referral component

---
+++ app/javascript/src/Components/Referral.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Referral = () => {
+    return (
+      <div >
+        Refer people you know :) 
+      </div>
+    );
+}
+
+export default Referral;
