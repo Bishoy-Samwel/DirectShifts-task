0797bae180c38f73a9e61f13d4c47c71bff6e812
Bishoy <bishoy.samwuel@gmail.com>
2022-06-01 16:58:26 +0200

    Prpare the list of the invited users

--- app/controllers/referral_controller.rb
+++ app/controllers/referral_controller.rb
@@ -1,9 +1,19 @@
 class ReferralController < ApplicationController
   before_action :authenticate_user!
+
+  def referred_users_status(referred_users)
+    referred_users_status = []
+    current_user.referred_users.each do |user|
+      status = user.referral_completed_at? ? "complete" : "pending"
+      referred_users_status.push({user:user, status:status})
+    end
+    referred_users_status
+  end
+
   def show
     render json: {
       message: "Get the list of referred emails #{current_user.email}",
-      referred: current_user.referred_users
+      referred_users_status: referred_users_status(current_user.referred_users)
     }
   end
 end
