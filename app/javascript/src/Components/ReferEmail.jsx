9027a08cd4c1aa4af0f7737ef07ed8efab444eba
Bishoy <bishoy.samwuel@gmail.com>
2022-06-03 19:47:40 +0200

    Style ReferEmail

--- app/javascript/src/Components/ReferEmail.jsx
+++ app/javascript/src/Components/ReferEmail.jsx
@@ -1,5 +1,8 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import Box from '@mui/material/Box';
+import  TextField  from '@mui/material/TextField';
+import  Button  from '@mui/material/Button';
 
 export const ReferEmail = (props) => {
   const form = useRef();
@@ -20,10 +23,33 @@ export const ReferEmail = (props) => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <input type="submit" value="Send" />
-    </form>
+    // <form ref={form} onSubmit={sendEmail}>
+    //   <label>Email</label>
+    //   <input type="email" name="user_email" />
+    //   <input type="submit" value="Send" />
+    // </form>
+
+    <Box component="form" ref={form} onSubmit={sendEmail} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              type="email"
+              label="Email Address"
+              name="user_email" 
+              autoComplete="email"
+              autoFocus
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Send
+            </Button>
+          </Box>
   );
 };
